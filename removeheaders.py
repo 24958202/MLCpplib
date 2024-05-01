7fe81682dcb03edf2fcef1e9c91e569a6e00b934
24958202 <24958202@qq.com>
2024-05-02 04:09:05 +0800

    Update removeheaders.py

--- removeheaders.py
+++ removeheaders.py
@@ -9,14 +9,21 @@ def remove_lines(file_path, x, y):
     with open(file_path, 'w') as file:
         file.writelines(lines[x:-y])
 
-folder_path = './books'
+def convert_to_utf8(file_path):
+    with open(file_path,'r',encoding='utf-8') as file:
+        content = file.read()
+
+    with open(file_path,'w',encoding='utf-8') as file:
+        file.write(content)
+
+folder_path = '/home/ronnieji/corpus/book'
 x = 26  # number of lines to remove from the top
 y = 351  # number of lines to remove from the bottom
 
 for file_name in os.listdir(folder_path):
     if file_name.endswith('.txt'):
         file_path = os.path.join(folder_path, file_name)
+        convert_to_utf8(file_path)
         remove_lines(file_path, x, y)
 
 print("Done!");
-
