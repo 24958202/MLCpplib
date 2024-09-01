f975f60385210e6ccaeb3833d890f79113eb3574
24958202 <dengfeng@hotmail.com>
2024-09-01 19:08:32 +0800

    Add files via upload

--- SpeechRecognitionWrapper.mm
+++ SpeechRecognitionWrapper.mm
@@ -11,21 +11,23 @@ public:
 
     void startRecognition() {  
         @autoreleasepool {  
+            __block bool authorized = false;  
             [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {  
                 if (status != SFSpeechRecognizerAuthorizationStatusAuthorized) {  
                     std::cerr << "Speech recognition not authorized." << std::endl;  
                     return;  
                 }  
+                authorized = true;  
             }];  
-
+            if (!authorized) {  
+                return;  
+            }  
             NSLocale *locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en-US"];  
             SFSpeechRecognizer *speechRecognizer = [[SFSpeechRecognizer alloc] initWithLocale:locale];  
-
             if (!speechRecognizer) {  
                 std::cerr << "Speech recognizer not available for the specified locale." << std::endl;  
                 return;  
             }  
-
             SFSpeechAudioBufferRecognitionRequest *request = [[SFSpeechAudioBufferRecognitionRequest alloc] init];  
             AVAudioEngine *audioEngine = [[AVAudioEngine alloc] init];  
             AVAudioInputNode *inputNode = [audioEngine inputNode];  
@@ -33,36 +35,31 @@ public:
             [inputNode installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer *buffer, AVAudioTime *when) {  
                 [request appendAudioPCMBuffer:buffer];  
             }];  
-
             [audioEngine prepare];  
             NSError *error;  
             if (![audioEngine startAndReturnError:&error]) {  
                 std::cerr << "Audio engine couldn't start: " << error.localizedDescription.UTF8String << std::endl;  
                 return;  
             }  
-
             [speechRecognizer recognitionTaskWithRequest:request resultHandler:^(SFSpeechRecognitionResult *result, NSError *error) {  
                 if (result) {  
                     recognizedText = result.bestTranscription.formattedString.UTF8String;  
                     std::cout << "Recognized Text: " << recognizedText << std::endl;  
                 }  
-
                 if (error) {  
                     std::cerr << "Recognition error: " << error.localizedDescription.UTF8String << std::endl;  
                 }  
-
                 if (result.isFinal) {  
                     std::cout << "Final recognized text: " << recognizedText << std::endl;  
                     [audioEngine stop];  
                     [inputNode removeTapOnBus:0];  
+                    CFRunLoopStop(CFRunLoopGetCurrent());  
                 }  
             }];  
-
             std::cout << "Listening... Press Ctrl+C to stop." << std::endl;  
-            [[NSRunLoop currentRunLoop] run];  
+            CFRunLoopRun();  
         }  
-    }  
-
+    }
     std::string getRecognizedText() {  
         return recognizedText;  
     }  
